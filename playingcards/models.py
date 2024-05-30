6e37742a6373afed0eb43476d4479ef176eab4c5
bloodyfork <mohtor.138055@gmail.com>
2024-05-30 23:39:49 +0430

    created model for cards

--- playingcards/models.py
+++ playingcards/models.py
@@ -1,3 +1,33 @@
 from django.db import models
-
+from core.models import BaseModel
 # Create your models here.
+
+
+class Card(BaseModel):
+    NUMBER_CHOICE = {
+
+        '1': 'ace',
+        '2': '2',
+        '3': '3',
+        '4': '4',
+        '5': '5',
+        '6': '6',
+        '7': '7',
+        '8': '8',
+        '9': '9',
+        '10': '10',
+        '11': 'jack',
+        '12': 'queen',
+        '13': 'king',
+
+         }
+
+    number = models.CharField(choices=NUMBER_CHOICE)
+    type_card = models.ForeignKey('TypeCard', on_delete=models.CASCADE)
+
+
+class TypeCard(BaseModel):
+    SYMBOL_CHOICE = {'spade': 'spade', 'club': 'club', 'diamond': 'diamond', 'heart': 'heart'}
+    COLOR_CHOICE = {'black': 'black', 'red': 'red'}
+    symbol = models.CharField(choices=SYMBOL_CHOICE)
+    color = models.CharField(choices=COLOR_CHOICE)
